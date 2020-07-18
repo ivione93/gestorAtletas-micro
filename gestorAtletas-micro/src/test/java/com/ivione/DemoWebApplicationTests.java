8f4c84fb2fdc9dac4c0722513d83eb88e8d61388
Iván García <ivgarciag@yahoo.es>
2020-07-18 12:25:30 +0200

    Cambio puerto BBDD

--- gestorAtletas-micro/src/test/java/com/ivione/DemoWebApplicationTests.java
+++ gestorAtletas-micro/src/test/java/com/ivione/DemoWebApplicationTests.java
@@ -1,7 +1,5 @@
 package com.ivione;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 /*import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
